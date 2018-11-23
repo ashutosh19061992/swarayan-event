3bfc4a2702a754223faf9b4deea55cfb9b93cb33
ashutosh19061992 <ashutoshadarsh45@gmail.com>
2018-11-23 16:58:44 +0530

    made final changes

--- app/models/concert.rb
+++ app/models/concert.rb
@@ -1,6 +1,6 @@
 class Concert < ApplicationRecord
   has_many :attendances
-  belongs_to :location, :dependent => :destroy   
+  belongs_to :location, :dependent => :delete
   has_many :follow_ups  
   has_many :artists, :dependent => :destroy 
   has_and_belongs_to_many :subscriptions  
